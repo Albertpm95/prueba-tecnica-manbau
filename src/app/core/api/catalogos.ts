c4e1470936e9a1647622843804b1bf88d9b163f5
Albert <Albertpm95@gmail.com>
2026-03-19 09:39:46 +0100

    Corregido el string de la url de los catalogos.

--- src/app/core/api/catalogos.ts
+++ src/app/core/api/catalogos.ts
@@ -1,5 +1,7 @@
+import { environment } from 'environments/environments';
+
 export const CATALOGOS_API = {
-  base: '${environment.apiUrl}',
+  base: `${environment.apiUrl}`,
   estados: () => `${CATALOGOS_API.base}/estados`,
   prioridades: () => `${CATALOGOS_API.base}/priorities`,
 };
